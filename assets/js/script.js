4dba5531da64952505568c7476d0b9910d29703f
Karolis <k.petrauskas92@gmail.com>
2023-02-21 14:21:22 +0000

    add modal for signup + styling + functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 
-/**
- * Accordion function
- */
+// accordion
 
 let accordionButtons = document.querySelectorAll('.accordion-button');
 let activeButton = null;
@@ -27,3 +25,37 @@ accordionButtons.forEach(button => {
 
   });
 });
+
+// email sign up
+
+let form = document.getElementById('sign-up__form');
+let alertModal = document.getElementById('alert-modal');
+let message = alertModal.querySelector('#alert-message');
+let closeBtn = alertModal.querySelector('.alert-close');
+
+function showAlert(messageText) {
+  message.textContent = messageText;
+  message.innerHTML = `<h2 style="color: white;">${messageText}</h2>`;
+  alertModal.style.display = 'block';
+}
+
+function closeAlert() {
+  alertModal.style.display = 'none';
+}
+
+closeBtn.addEventListener('click', closeAlert);
+window.addEventListener('click', function(event) {
+  if (event.target == alertModal) {
+    closeAlert();
+  }
+});
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let nameInput = form.querySelector('input[name="name"]');
+  let name = nameInput.value;
+  showAlert(`Thank you for subscribing to Backpack Buddy, ${name}!`);
+}
+
+form.addEventListener('submit', handleFormSubmit);
